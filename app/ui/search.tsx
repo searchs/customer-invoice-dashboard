8a19252ab3142f9c8c29df4f7b528c3c3c8fd977
searchs <ola.ajibode@gmail.com>
2024-04-16 15:42:45 +0100

    Paginate search results

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,6 +14,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    
     if(term){
       params.set('query', term)
     }else {
@@ -23,7 +25,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 replace(`${pathname}?${params.toString()}`)
 
   }, 300);
-  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
